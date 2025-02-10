9e290452e85b80c1d72c88958ab9be406fe6c8b8
Keerthana Kallayil Surendran <keerthana.kallayilsurendran@gmail.com>
2025-02-11 00:53:57 +0530

    Completed

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiService {
-  server_url = 'http://localhost:3000';
+  server_url = 'https://grabit-backend.onrender.com';
 
   constructor(private http: HttpClient) {}
 
